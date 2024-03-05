bd349809074330b4bdcde667764910e1eb37f4bb
PatilDarsh <darshpatil072@gmail.com>
2024-03-05 19:40:49 +0530

    [MODIFIED]:Darshan- Setting up Stack And Bet

--- src/com/infogalaxy/gameblingsimulation/GameblingSimulation.java
+++ src/com/infogalaxy/gameblingsimulation/GameblingSimulation.java
@@ -1,10 +1,19 @@
 package com.infogalaxy.gameblingsimulation;
 
 public class GameblingSimulation {
+	
+	private static final int STACK_PER_DAY = 100;
+	public static final int BET_PER_GAME = 1 ;
+	
+	public static void gameStatus() {
+		System.out.println("Stack Available :"+STACK_PER_DAY);
+		System.out.println("Bet Per Game : "+BET_PER_GAME);
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling SImulation Problem developed by Darshan Patil .");
-
+		
+		gameStatus();
 	}
 
 }

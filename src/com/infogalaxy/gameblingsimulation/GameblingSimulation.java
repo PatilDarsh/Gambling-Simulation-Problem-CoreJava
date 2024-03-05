167702b69b1008b5de2561e089d45769ee76ba5f
PatilDarsh <darshpatil072@gmail.com>
2024-03-05 19:26:40 +0530

    [ADDED]:Darshan - Gambling Simulation Setup

---
+++ src/com/infogalaxy/gameblingsimulation/GameblingSimulation.java
@@ -0,0 +1,10 @@
+package com.infogalaxy.gameblingsimulation;
+
+public class GameblingSimulation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambling SImulation Problem developed by Darshan Patil .");
+
+	}
+
+}

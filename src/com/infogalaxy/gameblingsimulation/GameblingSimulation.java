7c1751dd6d92be89944a041dc529789b82f6e9cb
PatilDarsh <darshpatil072@gmail.com>
2024-03-06 19:41:43 +0530

    [MODIFIED]:Darshan:Game For Day

--- src/com/infogalaxy/gameblingsimulation/GameblingSimulation.java
+++ src/com/infogalaxy/gameblingsimulation/GameblingSimulation.java
@@ -18,18 +18,30 @@ public class GameblingSimulation {
 		System.out.println("Bet Per Game : "+BET_PER_GAME);
 		System.out.println("Winning Stack :"+winStack);
 		System.out.println("Looseing Stack :"+looseStack);
+		System.out.println("------------------------------");
+		
 	}
 	
 	public static void gamePlay() {
-		Random random = new Random();
-		int playStatus = random.nextInt(9)%2 ;
-		System.out.println("Play Status :"+playStatus);
-		if(playStatus == LOOSE) {
-			looseStack += 1;
+		while(true) {
+			gameStatus();
 			
-		}
-		else {
-			winStack += 1 ;
+			Random random = new Random();
+			int playStatus = random.nextInt(9)%2 ;
+			System.out.println("Play Status :"+playStatus);
+			if(playStatus == LOOSE) {
+				looseStack += 1;
+			
+			}
+			else {
+				winStack += 1 ;
+			}
+			if(winStack == STACK_PER_DAY/2) {
+				break;
+			}
+			if(looseStack == STACK_PER_DAY/2) {
+				break;
+			}
 		}
 		
 	}
@@ -37,9 +49,9 @@ public class GameblingSimulation {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling SImulation Problem developed by Darshan Patil .");
 		
-		gameStatus();
+	
 		gamePlay();
-		gameStatus();
+		
 	}
 
 }

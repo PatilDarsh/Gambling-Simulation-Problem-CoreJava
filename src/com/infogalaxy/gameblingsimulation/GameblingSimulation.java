f16fcb3082e85647b88b65e281370cd1eba28ae8
PatilDarsh <darshpatil072@gmail.com>
2024-03-06 19:10:16 +0530

    [MODEFIED]:Game Play

--- src/com/infogalaxy/gameblingsimulation/GameblingSimulation.java
+++ src/com/infogalaxy/gameblingsimulation/GameblingSimulation.java
@@ -1,18 +1,44 @@
 package com.infogalaxy.gameblingsimulation;
 
+import java.util.Random;
+
 public class GameblingSimulation {
 	
 	private static final int STACK_PER_DAY = 100;
 	private static final int BET_PER_GAME = 1 ;
+	private static final int WIN = 1 ;
+	private static final int LOOSE = 0 ;
+	
+	static int winStack = 0 ;
+	static int looseStack = 0 ;
+	
 	
 	public static void gameStatus() {
 		System.out.println("Stack Available :"+STACK_PER_DAY);
 		System.out.println("Bet Per Game : "+BET_PER_GAME);
+		System.out.println("Winning Stack :"+winStack);
+		System.out.println("Looseing Stack :"+looseStack);
+	}
+	
+	public static void gamePlay() {
+		Random random = new Random();
+		int playStatus = random.nextInt(9)%2 ;
+		System.out.println("Play Status :"+playStatus);
+		if(playStatus == LOOSE) {
+			looseStack += 1;
+			
+		}
+		else {
+			winStack += 1 ;
+		}
+		
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling SImulation Problem developed by Darshan Patil .");
 		
+		gameStatus();
+		gamePlay();
 		gameStatus();
 	}
 

e45b5929785c12d2b3f1582c05b1fc9f7326d20f
PatilDarsh <darshpatil072@gmail.com>
2024-03-16 11:38:35 +0530

    [ADDED]:Darshan: Calculated Total Winning Days And Total Loosing Days in a Month

--- src/com/infogalaxy/gameblingsimulation/GameblingSimulation.java
+++ src/com/infogalaxy/gameblingsimulation/GameblingSimulation.java
@@ -4,56 +4,60 @@ import java.util.Random;
 
 public class GameblingSimulation {
 	
-	private static final int STACK_PER_DAY = 100;
-	private static final int BET_PER_GAME = 1 ;
-	private static final int WIN = 1 ;
-	private static final int LOOSE = 0 ;
-	
-	 
-	
-	public static void gameStatus() {
-		System.out.println("Stack Available :"+STACK_PER_DAY);
-		System.out.println("Bet Per Game : "+BET_PER_GAME);
-
-	}
-	
-	public static void gamePlay() {
-		Random random = new Random();
-		for(int i=1 ; i <=20 ; i++) {
-			System.out.println("Day:"+i);
-			int winStack = 0 ;
-			int looseStack = 0 ;
-			while(true) {
-				
-				int playStatus = random.nextInt(9)%2 ;
-				if(playStatus == LOOSE) {
-					looseStack += 1;
-				
+		private static final int STAKE_PER_DAY = 100;
+		private static final int BET_PER_GAME = 1;
+		private static final int WIN = 1;
+		private static final int LOOSE = 0;
+		
+		public static void gameStatus() {
+//			System.out.println("Stack Available: " +STAKE_PER_DAY);
+//			System.out.println("Bet per game: " +BET_PER_GAME);	
+		}
+		
+		public static void gamePlay() {
+			Random random = new Random();
+			int[] winAmount = new int[20];
+			int[] looseAmount = new int[20];	
+		
+			int wonDays = 0;
+			int lostDays = 0;
+			for(int i = 0; i < 20; i++) {
+				System.out.println("Day: " +(i+1));
+				int winStack = 0;
+				int looseStack = 0;
+				while(true) {
+					int playStatus = random.nextInt(9) % 2;
+					
+					if(playStatus == LOOSE) {
+						looseStack += 1;
+					} else {
+						winStack += 1;			
+					}
+					
+					if(looseStack == STAKE_PER_DAY / 2) {
+						lostDays++;
+						break;
+					}
+					if(winStack == STAKE_PER_DAY / 2) {
+						wonDays++;
+						break;
+					}
+					
 				}
-				else {
-					winStack += 1 ;
-				}
-				if(winStack == STACK_PER_DAY/2) {
-					break;
-				}
-				if(looseStack == STACK_PER_DAY/2) {
-					break;
-				}
+				looseAmount[i] = looseStack;
+				winAmount[i] = winStack;
+				System.out.println("Loose Amount: " +looseAmount[i]);
+				System.out.println("Win Amount: " +winAmount[i]);
+				System.out.println("------------------------");	
 			}
-			System.out.println("Winning Stack:"+winStack );
-			System.out.println("Looseing Stack:"+looseStack);
-			System.out.println("--------------------------------");
-			
-			
-		}	
-	}
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to Gambling SImulation Problem developed by Darshan Patil .");
+			System.out.println("Total Days Lost: " +lostDays);
+			System.out.println("Total Days Won: " +wonDays);
+		}
 		
-	
-		gamePlay();
 		
-	}
+		public static void main(String[] args) {
+			System.out.println("Welcome to Gambling Simulation Problem developed by Darshan Patil");
+			gamePlay();
+		}
 
-}
+	}
